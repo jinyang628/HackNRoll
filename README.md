e7960b2edc9915691930a701a4ab09debf5d0322
Jin Yang <81862152+jinyang628@users.noreply.github.com>
2023-01-14 17:17:48 +0800

    commit 7

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Backend deployment (cmd line)
 1. cd backend
-2. pip install -r requirements
+2. pip install -r requirements.txt
 3. uvicorn main:app --reload
--- gptspeaks/src/Login.tsx
+++ gptspeaks/src/Login.tsx
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  const data = {
+    "username": username,
+    "email": email,
+    "password": password
+  };
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-    // Perform login or registration logic here
-    // ...
+    axios.post('http://127.0.0.1:8000/register', data)
+    .then((response: any) =>{
+      console.log(response);
+    })
+    .catch((error: any) => {
+      console.log(error);
+    });
   };
 
   return (
     <form className="regLogForm" onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input value={username} onChange={(e) => setUsername(e.target.value)} required />
+      </label>
+      <br />
       <label>
         Email:
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
