dcc26469d49b34fa27df0ef9e321f318b2593519
JUN NENG <junneng@JUNs-MacBook-Air.local>
2023-01-15 01:32:01 +0800

    added getDetails changeDetails

--- gptspeaks/src/App.tsx
+++ gptspeaks/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import "./style.css";
 import Title from './FrontPage/Title';
@@ -6,13 +6,25 @@ import Login from './FrontPage/Login';
 import Record from './RecordPage/Record';
 import NavBar from './Common/NavBar';
 import PlayWAV from './PlayWAV';
+import Details from './RecordPage/Details';
+import BrowserRouter from {}
 
 function App() {
+  const [details, setDetails] = useState('');
+  const changeDetails = (e: any) => {
+    setDetails(e);
+  }
+
+  const getDetails = () => {
+    return details;
+  }
+
   return (
     <div>
       <NavBar/>
       <Title/>
-      <Login/>
+      <Login changeDetails={changeDetails}/>
+      <Details changeDetails={changeDetails} getDetails={getDetails}/>
       <Record/>
       <PlayWAV/>
     </div>
